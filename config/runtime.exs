59801eed7920abea25b787d38bb25225d26db121
Neven DREAN <nevendrean@yahoo.fr>
2023-10-27 15:53:38 +0200

    experiment bumblebee fly

--- config/runtime.exs
+++ config/runtime.exs
@@ -66,7 +66,7 @@ if config_env() == :prod do
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       # ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      ip: {127, 0, 0, 0},
+      ip: {0, 0, 0, 0, 0, 0, 0, 1},
       port: port
     ],
     secret_key_base: secret_key_base
